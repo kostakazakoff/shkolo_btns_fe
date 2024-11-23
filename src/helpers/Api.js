33bbaba6dcf6c4a66273e1a416a40737ab9c0012
Kosta Kazakov <kostakazakoff@gmail.com>
2024-11-23 22:16:45 +0200

    add origin environment in .env

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const origin = import.meta.env.VITE_API_BASE_URL
+? import.meta.env.VITE_API_BASE_URL
+: 'http://localhost:8000'
 
 const be = axios.create({
-    baseURL: 'http://localhost:8000/api/buttons',
+    baseURL: `${origin}/api/buttons`,
     withCredentials: true,
     withXSRFToken: true,
     headers: {
