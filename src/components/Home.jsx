a297052dcbd99a9c0c007283edbce95ea04b06a5
Kosta Kazakov <kostakazakoff@gmail.com>
2024-12-09 12:35:33 +0200

    Home.jsx - target _blank if link, css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
                         type="button"
                         className="btn"
                         to={data.link ? data.link : "#"}
-                        target="_blank"
+                        target={data.link && "_blank"}
                         style={{
                             backgroundColor: data.color ? data.color : "none",
                             borderColor: data.color && data.color,
